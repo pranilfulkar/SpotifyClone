6a5d809ab288550336b8352afd2c4fd93b187cd2
Pranil Fulkar <pranil.fulkar@fusionstak.com>
2025-06-24 16:03:07 +0530

    created using html, css and Javascript

---
+++ script.js
@@ -0,0 +1,117 @@
+console.log("Welcome to Spotify");
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+
+let songs = [
+    {songName: "Let Me Down Slowly", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Janji Heroes Tonight", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Raabta", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+]
+
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+
+})
+
+
+//handle play, pause button
+masterPlay.addEventListener('click', () =>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+
+        document.getElementById(songIndex).classList.remove('fa-circle-play');
+        document.getElementById(songIndex).classList.add('fa-circle-pause');
+
+
+    } else {
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+
+        document.getElementById(songIndex).classList.remove('fa-circle-pause');
+        document.getElementById(songIndex).classList.add('fa-circle-play');
+    }
+})
+
+
+//time update
+audioElement.addEventListener('timeupdate', () =>{
+    //updating seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+})
+
+
+//change song by seek
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-circle-pause');
+        element.classList.add('fa-circle-play');
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=> {
+    element.addEventListener('click', (e)=> {
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-circle-play'); 
+        masterPlay.classList.add('fa-circle-pause');
+    })
+})
+
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex >=songs.length-1){
+        songIndex = 0;
+    } else {
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play'); 
+    masterPlay.classList.add('fa-circle-pause');
+    gif.style.opacity = 1;
+})
+
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex <= 0){
+        songIndex = songs.length-1;
+    } else {
+        songIndex -= 1;
+    }
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play'); 
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+})
